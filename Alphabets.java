eb837ab3496f27bd6fd3c68463f4aadae04d2e4d
Ammudivya <41510010+Ammudivya@users.noreply.github.com>
2018-09-03 18:54:21 +0530

    Update and rename AlphabetOrNot.java to Alphabets.java

--- Alphabets.java
+++ Alphabets.java
@@ -1,8 +1,8 @@
-public class AlphabetOrNot {
+public class Alphabets{
 	public static void main(String args[]) {
 		Scanner sc=new Scanner (System.in);
-		char c=sc.next().charAt(0);
-		if(c>='a'&& c<='z'|| c>='A'&& c<='Z') {
+		char d=sc.next().charAt(0);
+		if(d>='a'&& d<='z'|| d>='A'&& d<='Z') {
 			System.out.println("Alphabet");
 		}
 		else {
