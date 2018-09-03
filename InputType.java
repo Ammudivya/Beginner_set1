afdaf5b95902be34b308649fd13c06b9164ec785
Ammudivya <41510010+Ammudivya@users.noreply.github.com>
2018-09-03 18:28:30 +0530

    Create  InputType.java

---
+++ InputType.java
@@ -0,0 +1,19 @@
+package Set1;
+import java.util.*;
+public class InputType {
+public static void main(String args[]) {	
+
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	 if(n<1) {
+		 System.out.println("negative");
+	 }
+	 else if(n>1) {
+		 System.out.println("Positive");
+	 }
+	 else {
+		 System.out.println("Zero");
+	 }
+
+}
+}
