7a163a5925d940839a9f84aa9adf84b27aabd11c
Ammudivya <41510010+Ammudivya@users.noreply.github.com>
2018-09-03 19:07:01 +0530

    Create LeapYear.java

---
+++ LeapYear.java
@@ -0,0 +1,13 @@
+public class LeapYear {
+	public static void main(String args[]) {
+		Scanner r=new Scanner (System.in) ;
+		int n=r.nextInt();
+		if(n%4==0) {
+			System.out.println("yes");
+		}
+		else {
+			System.out.println("No");
+		}
+
+}
+}
