033f3dbe1521d0f41ec94b183eb04d1f89721f08
Ammudivya <41510010+Ammudivya@users.noreply.github.com>
2018-09-04 18:47:07 +0530

    Update and rename Nk.java to Adding.java

---
+++ Adding.java
@@ -0,0 +1,19 @@
+public class Addind{
+ public static void main(String args[]) {
+	 Scanner sc=new Scanner(System.in);
+	 System.out.println("N");
+	 int n=sc.nextInt();
+	 System.out.println("K");
+	 int k=sc.nextInt();
+	 int a[]=new int[n];
+	 int sum=0;
+	  for(int i=0;i<a.length;i++) {
+		 a[i]=sc.nextInt();
+	 }
+	  for(int i=0;i<k;i++) {
+		      sum=a[i]+sum;
+	 }
+	  System.out.println(sum);
+	 sc.close();
+   }
+}
