89aa03c2829708e6c8980144d7a33bbd7f8b35eb
Ammudivya <41510010+Ammudivya@users.noreply.github.com>
2018-09-04 18:40:05 +0530

    Create Count.java

---
+++ Count.java
@@ -0,0 +1,25 @@
+public class Count {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int count=0;
+		while(n!=0) {
+			
+			n=n/10;
+		    count=count+1;
+		    
+			
+		}
+		System.out.println(count);
+	    
+        
+		
+			
+		
+		
+
+	}
+
+}
