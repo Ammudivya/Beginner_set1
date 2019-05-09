fad8925cac05e2c8eb2a6d3165de5005f9b44134
Ammudivya <41510010+Ammudivya@users.noreply.github.com>
2019-05-09 15:22:08 +0530

    Create multiplesOfNumber.java

---
+++ multiplesOfNumber.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int temp=0;
+        for(int i=1;i<=5;i++){
+        System.out.print(n*i+" ");
+        }
+    }
+}
